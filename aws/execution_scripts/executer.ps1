5001f45b0c56753327744d432305765f60cc6699
odedrafi <odedrafi74@gmail.com>
2022-06-28 00:07:55 +0300

    last working draft

--- aws/execution_scripts/executer.ps1
+++ aws/execution_scripts/executer.ps1
@@ -1,22 +1,26 @@
 # in this script we will set our location to the bin directory in it we will run our algorithm
-# we will start the EXE process on the samples located in the input folder
-# after we will recise the output of the algorithm run we will copy it to the s3 bucket were the output will be stored
-# in the end we will terminate the ec2 intance to save cost.
+# we will start the EXE process on the sample files located in the input folder
+# after producing an output, we will copy it back to the s3 bucket
+# at the end we will terminate the ec2 intance
+
+# define local path directories in variables
+$Local_output_path = "C:\nucleix\output"
+$Local_input_path = "C:\nucleix\input"
+$Local_algo_bin_path = "C:\nucleix\bin"
 
 
-
-# setting location to the directory were the EXE process is lcated
-Set-Location -Path C:\nucleix\bin
+# setting location to the directory were the EXE process is lcated so we could run the process
+Set-Location -Path $Local_algo_bin_path
 
 # looping over the samples in the local input folder
-foreach($SampleFile in Get-ChildItem "C:\nucleix\input"){ 
+foreach($SampleFile in Get-ChildItem $Local_input_patht){ 
     
-.\Nucleix.BladderEpicheck.V1_9_51.exe C:\nucleix\input\$SampleFile $env:ALGORITHM_NAME C:\nucleix\output
+.\Nucleix.BladderEpicheck.V1_9_51.exe $Local_input_path\$SampleFile $env:ALGORITHM_NAME $Local_output_path
     
 }
 
 
-# #wait for service output(files in local service output folder) and copy it back to s3 bucket
+# #wait for algo output, and copy it back to s3 bucket
 # aws s3 cp C:\nucleix\output $Bucket_Data_Output_Path
 
 
--- aws/master_script.ps1
+++ aws/master_script.ps1
@@ -1,4 +1,14 @@
-
+# master script
+# this script is as part of a workflow that includes :
+# a SNS trigger by a file upload to a s3 bucket building a ec2 instance with env variables defind with 
+# parameters nedded for the script to run
+# resulte=> installing an algorithm and executing it with a sample file inorder to produce an output 
+# and then copying it to a s3 bucket
+# 
+# to echieve that the master script wil run two sub scripts
+# 1. install_algo_script.ps1 - this script will install(copy) the relevent algo to the local instance
+# 2. executer.ps1- this script will copy the sample file from the s3 bucket and execute the algo on the sample file
+# produce an output, copy it back to the s3 bucket and terminate the instance.
 
 
 # set execution policy inorder to execute scripts
