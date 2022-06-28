546cde24a4df5daff1c025b12473a9fd9bb7ee3b
Oded Rafi <Odedrafi74@gmail.com>
2022-06-28 11:20:15 +0300

    working vers

--- aws/master_script.ps1
+++ aws/master_script.ps1
@@ -10,17 +10,17 @@
 # 2. executer.ps1- this script will copy the sample file from the s3 bucket and execute the algo on the sample file
 # produce an output, copy it back to the s3 bucket and terminate the instance.
 
-
+Set-Location -Path  "C:\scripts"
 # set execution policy inorder to execute scripts
 Set-ExecutionPolicy RemoteSigned
 
 
 #####################################################################################
 # service installation script.will copy the algoritem from s3 bucket to ec2 instance
- & .\install_algo_script.ps1
+& .\install_algo_script.ps1
 #####################################################################################
 
 #####################################################################################
 # running the script that will excute the algoritem on the samplesfrom s3 bucket inpput folder and return the service output to the s3 bucket
- & .\executer.ps1
+& .\executer.ps1
 #####################################################################################
